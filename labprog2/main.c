208655ebafe8b43dca4354a9ef936dc4ae81bdbf
wambela <wanl16te@cmrit.ac.in>
2016-09-16 13:17:57 +0530

    Update main.c

--- labprog2/main.c
+++ labprog2/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("Hello world!\n");
+int main ()
+   {
+     int a,rev=0,temp,mod;
+     
+     printf("enter the number to reverse\n");
+     scanf("%d",&a);
+     temp=a;
+     while (temp!=0)
+     
+       { 
+         mod=temp%10;
+         rev=(rev*10)+mod;
+         temp=temp/10;
+       }
+         if(a==rev)
+         printf("%d is a palindrome",a);
+        else
+         printf("%d is not a palindrome",a);
     return 0;
-}
+  }
+  
